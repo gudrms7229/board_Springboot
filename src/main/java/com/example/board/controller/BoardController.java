e7d6753374a37deed7701208ae2689ff4c6dc865
HyeongGeun You <yoo0211@naver.com>
2024-08-06 13:23:11 +0900

    2. 게시글 작성 및 my batis 세팅

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -1,12 +1,26 @@
 package com.example.board.controller;
 
 
+import com.example.board.dto.BoardDTO;
 import com.example.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+    return "save";
+}
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO){
+        System.out.println("boardDTO = " + boardDTO);
+        return "index";
+    }
 }
